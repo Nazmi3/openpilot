19b71c4c9d06d02fae6ed7fcebbee01491d02c50
nazmi <nazmiryuki@gmail.com>
2024-01-06 16:24:52 +0800

    yooy

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -22,6 +22,11 @@ const int UI_HEADER_HEIGHT = 420;
 
 const int UI_FREQ = 20; // Hz
 const int BACKLIGHT_OFFROAD = 50;
+
+const int bdr_s = 30;
+const int header_h = 420;
+const int footer_h = 280;
+
 typedef cereal::CarControl::HUDControl::AudibleAlert AudibleAlert;
 
 const float MIN_DRAW_DISTANCE = 10.0;
